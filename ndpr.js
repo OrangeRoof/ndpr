0675d1a007ef92f40d12d1759f6cbb94ae6f769b
holix <alch@alch.dev>
2020-08-05 01:32:43 -0700

    newest js source

--- ndpr.js
+++ ndpr.js
@@ -1,33 +1,30 @@
 'use strict';
 
-// warpAffine speedup: NEED TO USE LOCAL REGION OF ROTATION AFTER CIRCLE DETECTED
-// copy local region into black mask afterwards for draw
 // look into: mat.estimateRigidTransform
 // look into: webworkers
 
 const changeInstruction = text => $( '#status' ).first().html(text)
-const onOpenCvReady = () => changeInstruction('OpenCV.js is ready. Input RPM of rotating frame, then select movie:')
+const onOpenCvReady = () => changeInstruction('OpenCV.js is ready. Select movie:')
 const sleep = m => new Promise(r => setTimeout(r, m))
 
 var center = {x: 0, y: 0} // set to values during radius spec (radii = 0) and by circle detection
 var mouse = {x: 0, y: 0} // mouse values during mousedown before mouseup
 var radii = undefined // set to values by user input and after circle
-var frame_begin = Date.now()
+var frame_time = Date.now()
 var mat = undefined
 var mat_dup = undefined
 var mat_tmp = undefined
 var mat_tmp_tmp = undefined
-var rotMats = []
-var nth
+var rot_mat
 var vid
 var drawables
-var framesPerRotation = 0
+var dAngle
 var roiRect = undefined
 
 // Draw-centric functions
 const pullFrame = () => {
-	frame_begin = Date.now()
-	nth = Math.round($('#videoIn')[0].currentTime * 30) % framesPerRotation
+	frame_time = Date.now()
+	dAngle = - (($('#videoIn')[0].currentTime / (60 / Number($('#rpm')[0].value))) % 1) * 360.0
 	vid.read(mat)
 }
 
@@ -43,7 +40,6 @@ const drawMaskedDerotate = () => {
 	return new Promise(async r => {
 		cutMat()
 		warpMatTmp()
-		//resizeMat()
 		r()
 	})
 }
@@ -55,7 +51,10 @@ const drawMat = async () => {
 		} else {
 			cv.imshow('canvasOut', mat)
 		}
-		await sleep(1000/30 - (Date.now() - frame_begin))
+		frame_time = Date.now() - frame_time
+		if (frame_time < 29){
+			await sleep(1000/30 - frame_time)
+		}
 		r()
 	})
 }
@@ -65,7 +64,13 @@ const drawLoop = async () => {
 	while(true) {
 		pullFrame()
 		await Promise.all(drawables.map(f => f.apply(null)))
-		await drawMat()
+		if (mat_tmp_tmp != undefined) {
+			cv.imshow('canvasOut', mat_tmp_tmp)
+		} else {
+			cv.imshow('canvasOut', mat)
+		}
+		frame_time = Date.now() - frame_time
+		await sleep(1000/30 - frame_time)
 	}
 }
 
@@ -87,10 +92,9 @@ const detectCircle = () => {
 		changeInstruction("Could not detect circle. Try again: Click-drag from center of rotation frame to edge of rotation frame.")
 	} else {
 		makeBitMask()
-		buildRotMats()
 		drawables = [drawMaskedDerotate]
 		roiRect = new cv.Rect(Math.round(center.x - radii), Math.round(center.y - radii), Math.round(radii * 2), Math.round(radii * 2))
-		changeInstruction("Showing live preview of derotated video. Click \"Save\" to save derotated video, or \"Browse\" to change videos / reload and try detection again.")
+		changeInstruction("Showing live preview of derotated video. You can change the derotation RPM using the text box.")
 	}
 }
 
@@ -113,7 +117,8 @@ const cutMat = () => {
 }
 
 const warpMatTmp = () => {
-	cv.warpAffine(mat_tmp, mat_tmp, rotMats[nth], {width: 2*radii, height: 2*radii})
+	rot_mat = cv.getRotationMatrix2D({x: radii, y: radii}, dAngle, 1.0)
+	cv.warpAffine(mat_tmp, mat_tmp, rot_mat, {width: 2*radii, height: 2*radii})
 	mat_tmp.copyTo(mat_tmp_tmp, mat_dup)
 }
 
@@ -121,19 +126,10 @@ const resizeMat = () => {
 	cv.resize(mat_tmp_tmp, mat, {width: mat.cols, height: mat.rows})
 }
 
-const buildRotMats = () => {
-	// get frames per rot from 30 fps and x RPM
-	framesPerRotation = Math.round(1800 / Number($( '#rpm' ).attr('value')))
-	let dAngle = - 360.0/framesPerRotation
-	for(let i = 0; i < framesPerRotation; i++) {
-		rotMats.push(cv.getRotationMatrix2D({x: radii, y: radii}, dAngle * i, 1.0))
-	}
-}
-
 // Event Listeners
 $(window).on('load', () => {
 	$( '#fileInput' ).on('change', e => {
-		changeInstruction('Click-drag from center of rotation frame to edge of rotation frame.')
+		changeInstruction('Click-drag from center of rotation frame to edge of rotation frame.\n If nothing shows up, the video type is unsupported.')
 		$( '#videoIn' ).attr('src', URL.createObjectURL(e.target.files[0]))
 	})
 
@@ -162,17 +158,18 @@ $(window).on('load', () => {
 	})
 
 	$( '#canvasOut' ).on('mousedown', e => {
-		if(!radii) {
+		if(radii == 0) {
 			center.x = e.offsetX; center.y = e.offsetY; mouse.x = e.offsetX; mouse.y = e.offsetY
+			radii = -1
 	}})
 
 	$( '#canvasOut' ).on('mousemove', e => {
-		if(!radii && center.x && center.y) {
+		if(radii == -1 && center.x && center.y) {
 			mouse.x = e.offsetX; mouse.y = e.offsetY
 	}})
 
 	$( '#canvasOut' ).on('mouseup', e => {
-		if(!radii) {
+		if(radii == -1) {
 			radii = Math.sqrt(Math.pow(e.offsetX - center.x, 2) + 
 								Math.pow(e.offsetY - center.y, 2))
 			center.x = 0; center.y = 0; mouse.x = 0; mouse.y = 0;

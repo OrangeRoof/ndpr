c968ac0b5991184cb0d6f38575e7e518f73b5541
holix <alch@alch.dev>
2020-08-05 00:48:34 -0700

    speedup by reducing transform size

--- ndpr.js
+++ ndpr.js
@@ -3,6 +3,7 @@
 // warpAffine speedup: NEED TO USE LOCAL REGION OF ROTATION AFTER CIRCLE DETECTED
 // copy local region into black mask afterwards for draw
 // look into: mat.estimateRigidTransform
+// look into: webworkers
 
 const changeInstruction = text => $( '#status' ).first().html(text)
 const onOpenCvReady = () => changeInstruction('OpenCV.js is ready. Input RPM of rotating frame, then select movie:')
@@ -15,11 +16,13 @@ var frame_begin = Date.now()
 var mat = undefined
 var mat_dup = undefined
 var mat_tmp = undefined
+var mat_tmp_tmp = undefined
 var rotMats = []
 var nth
 var vid
 var drawables
 var framesPerRotation = 0
+var roiRect = undefined
 
 // Draw-centric functions
 const pullFrame = () => {
@@ -32,32 +35,38 @@ const drawRadiiVisual = () => {
 	return new Promise(async r => {
 		cv.line(mat, center, mouse, [255, 128, 0, 255], 1)
 		cv.circle(mat, center, Math.sqrt(Math.pow(mouse.x - center.x, 2) + Math.pow(mouse.y - center.y, 2)), [255, 128, 0, 255], 1)
+		r()
 	})
 }
 
 const drawMaskedDerotate = () => {
 	return new Promise(async r => {
-		maskMat()
-		warpMat()
+		cutMat()
+		warpMatTmp()
+		resizeMat()
+		r()
 	})
 }
 
-const callNextFrame = () => {
+const drawMat = async () => {
 	return new Promise(async r => {
-		await sleep(1000/30 - (Date.now() - frame_begin))
 		cv.imshow('canvasOut', mat)
+		await sleep(1000/30 - (Date.now() - frame_begin))
+		r()
 	})
 }
+
 
 const drawLoop = async () => {
-	pullFrame()
-	let x = await Promise.all(drawables.map(f => f.apply(null)))
-	drawLoop()
+	while(true) {
+		pullFrame()
+		await Promise.all(drawables.map(f => f.apply(null)))
+		await drawMat()
+	}
 }
 
 // OpenCV-centric functions
 const detectCircle = () => {
-	$( '#videoIn' )[0].pause()
 	changeInstruction("Analyzing...")
 	let circle = new cv.Mat()
 	mat_dup = cv.Mat.zeros(mat.cols, mat.rows, cv.CV_8U)
@@ -69,38 +78,43 @@ const detectCircle = () => {
 
 	if (isNaN(center.x + center.y + radii)) {
 		center.x = 0; center.y = 0; radii = 0;
-		drawables = [drawRadiiVisual, callNextFrame]
+		drawables = [drawRadiiVisual]
+		roiRect = undefined
 		changeInstruction("Could not detect circle. Try again: Click-drag from center of rotation frame to edge of rotation frame.")
 	} else {
-		$( '#videoIn' )[0].currentTime = 0
-		nth = 0
 		makeBitMask()
 		buildRotMats()
-		drawables = [drawMaskedDerotate, callNextFrame]
+		drawables = [drawMaskedDerotate]
+		roiRect = new cv.Rect(Math.round(center.x - radii), Math.round(center.y - radii), Math.round(radii * 2), Math.round(radii * 2))
 		changeInstruction("Showing live preview of derotated video. Click \"Save\" to save derotated video, or \"Browse\" to change videos / reload and try detection again.")
 	}
-	$( '#videoIn' )[0].play()
 }
 
 const makeBitMask = () => {
 	if (mat_tmp != undefined) {
 		mat_tmp.delete(); mat_tmp = undefined
 	}
-	mat_tmp = new cv.Mat(mat.cols, mat.rows, cv.CV_8UC4, [0, 0, 0, 255])
+	if (mat_tmp_tmp != undefined) {
+		mat_tmp_tmp.delete(); mat_tmp_tmp = undefined
+	}
+	mat_tmp = new cv.Mat(2*radii+5, 2*radii+5, cv.CV_8UC4, [0, 0, 0, 255])
+	mat_tmp_tmp = new cv.Mat(2*radii+5, 2*radii+5, cv.CV_8UC4, [0, 0, 0, 255])
 
-	mat_dup = cv.Mat.zeros(mat.cols, mat.rows, cv.CV_8U)
-	cv.circle(mat_dup, center, radii + 5, [255, 255, 255, 255], -1)
+	mat_dup = cv.Mat.zeros(2*radii+5, 2*radii+5, cv.CV_8U)
+	cv.circle(mat_dup, {x: radii, y: radii}, radii + 5, [255, 255, 255, 255], -1)
 }
 
-const maskMat = () => {
-	mat.copyTo(mat_tmp, mat_dup)
-	// do copy from mat_tmp back to mat along with warpAffine
-	// mat_tmp.copyTo(mat)
+const cutMat = () => {
+	mat_tmp = mat.roi(roiRect)
 }
 
-const warpMat = () => {
-	// todo: explicitly cache dims (?)
-	cv.warpAffine(mat_tmp, mat, rotMats[nth], {width: mat.rows, height: mat.cols}, cv.INTER_LINEAR, cv.BORDER_CONSTANT, [0, 0, 0, 255])
+const warpMatTmp = () => {
+	cv.warpAffine(mat_tmp, mat_tmp, rotMats[nth], {width: 2*radii+5, height: 2*radii+5})
+	mat_tmp.copyTo(mat_tmp_tmp, mat_dup)
+}
+
+const resizeMat = () => {
+	cv.resize(mat_tmp_tmp, mat, {width: mat.cols, height: mat.rows})
 }
 
 const buildRotMats = () => {
@@ -108,7 +122,7 @@ const buildRotMats = () => {
 	framesPerRotation = Math.round(1800 / Number($( '#rpm' ).attr('value')))
 	let dAngle = - 360.0/framesPerRotation
 	for(let i = 0; i < framesPerRotation; i++) {
-		rotMats.push(cv.getRotationMatrix2D(center, dAngle * i, 1.0))
+		rotMats.push(cv.getRotationMatrix2D({x: radii, y: radii}, dAngle * i, 1.0))
 	}
 }
 
@@ -135,7 +149,7 @@ $(window).on('load', () => {
 		}
 		vid = new cv.VideoCapture(vidIn)
 		mat = new cv.Mat(vidIn.height, vidIn.width, cv.CV_8UC4)
-		drawables = [drawRadiiVisual, callNextFrame]
+		drawables = [drawRadiiVisual]
 		// only call loop if first entry TODO: nicer once function
 		if (radii == undefined) {
 			drawLoop()
